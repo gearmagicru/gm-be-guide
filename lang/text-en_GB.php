39a2ceb4c4216eca985212dfd1e2fec1b9b4dd0a
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-17 20:56:43 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Guide',
+    '{description}' => 'User guide information',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Viewing guide information'],
+        'view' => ['View', 'Viewing guide information'],
+        'read' => ['Reading', 'Reading guide information']
+    ],
+
+    // Modal
+    'Guide' => 'Guide',
+
+    // Docs, TreeNodes: сообщения / ошибки
+    'It is impossible to gain access to the Help section' => 'It is impossible to gain access to the Help section.',
+    'Unable to determine help sections' => 'Unable to determine help sections.'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Справочная информация',
+    '{description}' => 'Справочная информация пользователя',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Просмотр справочной информации'],
+        'view' => ['Просмотр', 'Просмотр справочной информации'],
+        'read' => ['Чтение', 'Чтение справочной информации']
+    ],
+
+    // Modal
+    'Guide' => 'Справка',
+
+    // Docs, TreeNodes: сообщения / ошибки
+    'It is impossible to gain access to the Help section' => 'Невозможно получить доступ к разделу справки',
+    'Unable to determine help sections' => 'Невозможно определить разделы справки'
+];
