21b8a64148316547726bedc0d7f7179843301743
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-17 20:57:28 +0300

    Файл конфигурации нестроек модуля

---
+++ config/.settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации нестроек модуля.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'url' => 'https://docs.gearmagic.ru/'
+];
